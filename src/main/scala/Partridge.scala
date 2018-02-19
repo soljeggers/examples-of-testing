e83b51bea8b79f08095489c678bf87c9c6584cb5
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-02-19 14:58:30 +0000

    Added Unit Test 2 Added Solution for Unit Test 2

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,3 +1,13 @@
-class Partridge {
+object Partridge {
 
+  val terms : List[String] = List("PearTree")
+
+  def partridge(terms : List[String]) : String = {
+
+    if (terms == List("PearTree")) {
+      "Mine's a Pint!"
+    } else {
+      "Lynn, I've pierced my foot on a spike!!"
+    }
+  }
 }
--- src/test/scala/PartridgeSpec.scala
+++ src/test/scala/PartridgeSpec.scala
@@ -2,8 +2,19 @@ import org.scalatest.{MustMatchers, WordSpec}
 
 class PartridgeSpec extends WordSpec with MustMatchers {
 
-  
+  "Partridge" must  {
+
+    "return 'Lynn, I've pierced my foot on a spike!!'" in {
+      Partridge.partridge(List.empty) mustEqual "Lynn, I've pierced my foot on a spike!!"
+    }
+
+    "return 'Mine's a Pint!'" in {
+      Partridge.partridge(List ("PearTree") ) mustEqual "Mine's a Pint!"
+    }
 
 
 
+
+
+  }
 }
