c99a11ec45df6c6106af2f0952d64cab7ca03b0c
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-02-20 16:04:09 +0000

    Added Cypher Unit Tests Added Cypher Code

---
+++ src/main/scala/Cypher.scala
@@ -0,0 +1,21 @@
+object Cypher {
+
+  def apply(input: String, key: Int): List[Int] = {
+    val initialNumbers: List[Int] = input.map(char => letterToNumber(char)).toList
+
+    val zipped = initialNumbers.zip(splitKeys(key))
+    zipped.map(x => x._1 + x._2)
+  }
+
+  def letterToNumber(input: Char): Int = input.toInt - 96
+
+  def splitKeys(key: Int): List[Int] =
+    key.toString.map(x => x.asDigit).toList
+
+  def expandKeys(keys: List[Int], length: Int): List[Int] = {
+    val keyLength = keys.length
+    val timesToRepeat = length / keyLength
+    ???
+  }
+
+}
