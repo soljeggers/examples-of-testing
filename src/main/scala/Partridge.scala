78e29cc33d2ed1614ab02ea971e245cb2bbca7a0
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-02-20 09:54:18 +0000

    Added Pattern Matching Solution - Added New Tests

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,13 +1,25 @@
 object Partridge {
 
-  val terms : List[String] = List("PearTree")
+  val input : List[String] = List("PearTree", "Chat", "Partridge","Dan", "Toblerone", "Lynn", "Alphapapa", "Nomad")
+  val some = "Mine's a Pint!"
+  val none = "Lynn, I've Pierced my foot on a spike!!"
 
-  def partridge(terms : List[String]) : String = {
 
-    if (terms == List("PearTree")) {
-      "Mine's a Pint!"
-    } else {
-      "Lynn, I've pierced my foot on a spike!!"
-    }
+  def partridge(terms : List[String]) : String =
+    terms.count(x => input.contains(x)) match {
+  case 0 => "Lynn, I've pierced my foot on a spike!!"
+  case x => "Mine's a Pint" + "!" * x
+
   }
 }
+
+
+
+//
+//    if (terms.exists(input.contains(_))) {
+//      "Mine's a Pint!"
+//    } else {
+//      "Lynn, I've pierced my foot on a spike!!"
+//    }
+//  }
+
--- src/test/scala/PartridgeSpec.scala
+++ src/test/scala/PartridgeSpec.scala
@@ -2,19 +2,25 @@ import org.scalatest.{MustMatchers, WordSpec}
 
 class PartridgeSpec extends WordSpec with MustMatchers {
 
-  "Partridge" must  {
+  "Partridge" must {
 
-    "return 'Lynn, I've pierced my foot on a spike!!'" in {
+    "return 'Lynn, I've pierced my foot on a spike!!' if empty list" in {
       Partridge.partridge(List.empty) mustEqual "Lynn, I've pierced my foot on a spike!!"
     }
 
-    "return 'Mine's a Pint!'" in {
-      Partridge.partridge(List ("PearTree") ) mustEqual "Mine's a Pint!"
+    "return 'Mine's a Pint!' if term matches" in {
+      Partridge.partridge(List("PearTree")) mustEqual "Mine's a Pint!"
     }
 
+    "return 'Lynn, I've pierced my foot on a spike!! if only incorrect terms in list" in {
+      Partridge.partridge(List("Turkey")) mustEqual "Lynn, I've pierced my foot on a spike!!"
+    }
 
-
-
-
+    "return 'Mine's a Pint!' If two terms match" in {
+      Partridge.partridge(List("PearTree", "Toblerone")) mustEqual "Mine's a Pint!!"
+    }
+    "return' Mine's a Pint!!!!!!' If six terms match" in {
+      Partridge.partridge(List("PearTree", "Chat", "Partridge","Dan", "Toblerone", "Lynn"))
+    }
   }
 }
